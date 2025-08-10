61a03f8ba1ca45d8cfc067277af00e917e5bf0b7
David Cho <proust0319@gmail.com>
2025-08-10 11:56:28 +1000

    [feat]SMT-001: getArnieQuites upated

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,7 +2,19 @@ const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+
+  // Helper function to fetch the quote from a given URL
+  const fetchQuote = async (url) => {
+    const {status, body} = await httpGet(url)
+    // Parse the response body once
+    const parsedBody = JSON.parse(body);
+
+    // Return the quote or failure message based on the status
+    return status === 200 ? {'Arnie Quote': parsedBody.message} : {'FAILURE': parsedBody.message}
+  }
+
+  // Process all URLs concurrently and return the results
+  return await Promise.all(urls.map(fetchQuote));
 };
 
 module.exports = {
